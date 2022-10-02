1b8a14af5f183e322788e1e55d16fc62d23a7707
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2022-10-02 03:43:08 +0100

    first commit

---
+++ src/app/views/list/entree/entree.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Entree } from 'src/app/shared/interfaces/entree';
+@Component({
+  selector: 'app-entree',
+  templateUrl: './entree.component.html',
+  styleUrls: ['./entree.component.scss']
+})
+export class EntreeComponent implements OnInit {
+  @Input()
+  public entree: Entree;
+  constructor() {
+    this.entree = {
+      title: '',
+      abstract: ''
+    }
+  }
+  ngOnInit(): void {
+  }
+}
